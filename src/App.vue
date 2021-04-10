846e2f0399cb336fad56bb8f7d1948e8905cc9f3
Chantelsky <laver.chantelsky@gmail.com>
2021-04-10 18:12:11 +1000

    fix: added vue-router, active and hover classes

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="nav">
+    <!--TODO: if user is logged in view Navbar -->
+    <nav-bar />
+  </div>
+  <router-view />
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue';
+
+export default {
+  components: { NavBar },
+};
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <nav class="flex items-center justify-between h-17 fixed w-full font-sans">
+      <div class="flex inital m-7">
+        <img class="h-6 w-6" src="../assets/pinterest-logo.png" />
+      </div>
+      <div class="flex-inital text-md font-bold font-sans">
+        <!--TODO: set active class to when user is on page -->
+        <router-link
+          class="hover:bg-pinterestgray p-3 rounded-full active:bg-black active:text-white"
+          to="/"
+          >Home</router-link
+        >
+        <router-link
+          class="hover:bg-pinterestgray p-3 rounded-full active:bg-black active:text-white"
+          to="/today"
+          >Today</router-link
+        >
+        <router-view />
+      </div>
+      <div class="flex-grow mx-8">
+        <!--TODO: add icon in front of search, add functionality -->
+        <input
+          type="search"
+          class="bg-pinterestgray w-full p-3 rounded-full focus:outline-none focus:ring focus:border-blue-300"
+          name=""
+          id=""
+          placeholder="Search"
+        />
+      </div>
+      <div class="flex items-center justify-between w-1/12 mr-3 h-10">
+        <!--TODO: might need to find better icons-->
+        <div>
+          <img class="h-6" src="../assets/notification_icon.svg" />
+        </div>
+        <div>
+          <img class="h-6" src="../assets/chat_icon.svg" />
+        </div>
+        <div>
+          <!--TODO : avatar upload, for now this is placeholder-->
+          <img class="rounded-full h-6 w-6" src="../assets/person.png" />
+        </div>
+        <div>
+          <img class="h-6" src="../assets/arrow_down.svg" />
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
