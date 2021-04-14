6349b4a5fd507862c6ae8c3825604beacf074691
Chantelsky <laver.chantelsky@gmail.com>
2021-04-15 07:43:09 +1000

    fix: installed momentjs and added in date on today page

--- src/views/Today.vue
+++ src/views/Today.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="about flex flex-col items-center w-screen">
-    <h3 class="text-xl font-bold">{{}}</h3>
+    <h3 class="text-xl font-bold">{{ currentDateTime() }}</h3>
     <h1 class="text-4xl font-bold">Stay Inspired</h1>
   </div>
 </template>
 
 <script>
-export default {};
+import moment from 'moment';
+
+export default {
+  methods: {
+    currentDateTime() {
+      return moment().format('MMMM Do YYYY');
+    },
+  },
+};
 </script>
