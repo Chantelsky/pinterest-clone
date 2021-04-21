4de0d2fd72689bdc5a293a8c639ef7dc1f4fec38
Chantelsky <laver.chantelsky@gmail.com>
2021-04-22 08:39:41 +1000

    feat: adding in content for inbox

--- src/components/Inbox.vue
+++ src/components/Inbox.vue
@@ -2,6 +2,14 @@
   <div
     class="absolute right-0 -top-4.5 bg-white shadow-xl rounded-lg p-5 text-left w-1/5"
   >
-    Hello
+    <div class="flex items-center">
+      <div class="flex-grow"><p class="font-bold text-center">Inbox</p></div>
+      <button class="mr-4">
+        <img class="h-10" src="../assets/menu_icon.svg" />
+      </button>
+      <button>
+        <img class="h-6" src="../assets/edit_icon.svg" />
+      </button>
+    </div>
   </div>
 </template>
