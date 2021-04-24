cb914bc80c764b7deac79ad04ef328bb129dfd8b
Chantelsky <laver.chantelsky@gmail.com>
2021-04-24 23:20:40 +1000

    feat: added users to the inbox dropdown

--- src/components/Inbox.vue
+++ src/components/Inbox.vue
@@ -18,12 +18,40 @@
       </button>
     </div>
     <div>
-      <h1 class="font-bold text-3xl pt-2">Share ideas with your friends</h1>
+      <h1 class="font-bold text-4xl pt-2 mb-4">
+        Share ideas with your friends
+      </h1>
+      <input
+        class="border-2 border-pinterestgray rounded-full p-3 w-full cursor-pointer focus:outline-none focus:ring"
+        type="search"
+        placeholder="Search by name or email address"
+      />
+      <div class="mt-2">
+        <p class="text-sm text-darkgray pb-2">Suggested</p>
+        <ul>
+          <li
+            v-for="firstName in firstNames"
+            :key="firstName"
+            class="flex items-center pb-3 hover:bg-pinterestgray p-1"
+          >
+            <img
+              :src="pictures"
+              :alt="`${firstNames}`"
+              class="rounded-full flex"
+            />
+            <div class="pl-5 leading-none">
+              <p class="font-bold text-base">{{ firstNames }}</p>
+              <span class="text-xs text-darkgray">Following</span>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import IconBase from './IconBase.vue';
 import IconMenu from './icons/IconMenu.vue';
 import IconEdit from './icons/IconEdit.vue';
@@ -33,6 +61,19 @@ export default {
     IconBase,
     IconMenu,
     IconEdit,
+  },
+  data() {
+    return {
+      firstNames: '',
+      pictures: '',
+    };
+  },
+  mounted() {
+    axios.get('https://randomuser.me/api/?results=10').then((response) => {
+      this.$data.firstNames = response.data.results[0].name.first;
+      this.$data.pictures = response.data.results[0].picture.thumbnail;
+      console.log(response.data);
+    });
   },
 };
 </script>
