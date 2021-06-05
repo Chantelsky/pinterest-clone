935e5dbb6f8f7bafc4cb40091cf93f7f62a58bc2
Chantelsky <laver.chantelsky@gmail.com>
2021-06-05 20:37:17 +1000

    fix: display random message under users in inbox

--- src/components/InboxUsers.vue
+++ src/components/InboxUsers.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
     <ul>
-      <li v-for="user in allRandomUsers.results" :key="user" class="flex items-center pb-3 hover:bg-pinterestgray">
-        <img :src="`${user.picture.thumbnail}`" class="rounded-full mr-5"/>
+      <li
+        v-for="user in allRandomUsers.results"
+        :key="user"
+        class="flex items-center pb-3 hover:bg-pinterestgray"
+      >
+        <img :src="`${user.picture.thumbnail}`" class="rounded-full mr-5" />
         <div class="flex flex-col">
           <p class="font-bold text-base">{{ user.name.first }} {{ user.name.last }}</p>
-          <span class="text-xs text-darkgray">Following</span>
+          <span class="text-xs text-darkgray">{{message[Math.floor(Math.random() * message.length)]}}</span>
         </div>
       </li>
     </ul>
@@ -16,8 +20,17 @@
 import { mapGetters, mapActions } from 'vuex';
 
 export default {
-  methods: mapActions(['fetchRandomUsers']),
-  computed: mapGetters(['allRandomUsers']),
+  data() {
+    return {
+      message: ['In your network', 'Following'],
+    };
+  },
+  methods: {
+    ...mapActions(['fetchRandomUsers']),
+  },
+  computed: {
+    ...mapGetters(['allRandomUsers']),
+  },
   created() {
     this.fetchRandomUsers();
   },
